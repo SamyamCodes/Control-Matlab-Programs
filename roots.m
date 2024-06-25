5c2b49f06b9188a4736bce9096b1cc0590f46bc9
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-25 21:34:23 +0545

    Update roots.m

--- roots.m
+++ roots.m
@@ -1,3 +1,8 @@
 p=[1 9 31.25 61.25 67.75 14.75 15];
-r=roots(p);
+r=roots(p);   /* This is to find the roots from the given polynomials. */
 r;
+
+i=sqrt(-1);
+r=[-1 -2 -3+4*i -3-4*i];
+p=poly(r);
+p; /* This is to find the polynominal from the given roots. */
