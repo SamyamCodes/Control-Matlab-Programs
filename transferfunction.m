01117c9d2c45eff27d6ca98ca6268e58f793e1ab
Samyam Shrestha <82583939+SamyamCodes@users.noreply.github.com>
2024-06-25 21:41:43 +0545

    Update transferfunction.m

--- transferfunction.m
+++ transferfunction.m
@@ -2,3 +2,14 @@
 deno = [2 3 4 5 6 3 3 ];
  sys = tf(nume, deno);
  sys
+
+
+ num=[1 11 30 0];
+ den=[1 9 45 87 50];
+ [z,p,k]=tf2zp(num,den)
+
+ z=[-6;-5;0];
+>> k=1;
+>> i=sqrt(-1);
+>> p=[-3+4*i;-3-4*i;-2;-1];
+>> [num,den]=zp2tf(z,p,k)
